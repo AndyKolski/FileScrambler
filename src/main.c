886d8438c9910fbe8af051a28adb771183aa0e5e
AndyKolski <andykolski@gmail.com>
2017-12-18 22:52:30 -0700

    Improve readability

--- src/main.c
+++ src/main.c
@@ -7,20 +7,20 @@ int main(int argc, char *argv[]) {
 	char buffer;
 	long filelen;
 	float perc = 1;
-	int w = 0;
+	int offset = 0;
 	if (argc >= 4) {
 		perc = atof(argv[3]);
 		printf("Using chance of %f%%\n", perc);
 	}
 	if (argc == 5) {
-		w = atoi(argv[4]);
-		printf("Using byte offset of %iB\n", w);
+		offset = atoi(argv[4]);
+		printf("Using byte offset of %iB\n", offset);
 	}
 	if (argc < 4) {
 		printf("Usage: %s [Input File] [Output File] [Byte Change Chance %%] <Byte Offset>\n", argv[0]);
 		return 1;
 	}
-	if ( access(argv[1], F_OK) == -1 ) {
+	if (access(argv[1], F_OK) == -1) {
 			printf("Unable to open file \"%s\"\n", argv[1]);
 			return 1;
 	}
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
 	for (long i = 0; i < filelen; i++) {
 		buffer = 0;
 		fread(&buffer, 1, 1, sourcefile); 
-		if (i >= w && rand()/2147483647.0*100<perc) {
+		if (i >= offset && rand()/2147483647.0*100<perc) {
 			buffer = rand()/2147483647.0*255-128;
 		}
 		if (i % (long)(0.05*filelen) == 0) {
